aa61fe1432deef4ed0859daf857b7015ca651aea
Maximilian Beckmann <max@maximilian-beckmann.dev>
2025-07-17 18:13:57 +0200

    style groups overview

--- app/src/main/java/com/max/quotial/ui/component/GroupCard.kt
+++ app/src/main/java/com/max/quotial/ui/component/GroupCard.kt
@@ -1,20 +1,23 @@
 package com.max.quotial.ui.component
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.Icon
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.unit.dp
 import com.max.quotial.data.model.Group
 
@@ -25,8 +28,12 @@ fun GroupCard(
     onGroupClick: (String) -> Unit,
     onGroupJoin: (String) -> Unit,
 ) {
-    Card(
+    OutlinedCard(
         modifier = Modifier.fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.background
+        ),
+        border = BorderStroke(1.dp, Color.LightGray),
         onClick = { onGroupClick(group.id) },
     ) {
         Row(
@@ -34,16 +41,25 @@ fun GroupCard(
                 .fillMaxWidth()
                 .padding(16.dp),
             horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
+            verticalAlignment = Alignment.Top
         ) {
             Column {
                 Text(group.name)
+                Text(
+                    text = buildAnnotatedString {
+                        append(group.memberCount.toString())
+                        append(if (group.memberCount == 1) " member" else " members")
+                    },
+                    style = MaterialTheme.typography.bodySmall,
+                    color = MaterialTheme.colorScheme.outline
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
                 Text(group.description, color = MaterialTheme.colorScheme.outline)
             }
 
-            if (isMember) {
-                Icon(Icons.Default.Check, contentDescription = "joined")
-            } else {
+            if (!isMember) {
                 Button(onClick = { onGroupJoin(group.id) }) {
                     Text("join")
                 }
--- app/src/main/java/com/max/quotial/ui/screen/GroupsOverviewScreen.kt
+++ app/src/main/java/com/max/quotial/ui/screen/GroupsOverviewScreen.kt
@@ -3,6 +3,7 @@ package com.max.quotial.ui.screen
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -10,12 +11,14 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -34,7 +37,9 @@ fun GroupsOverviewScreen(
     Column {
         if (groups.isNotEmpty()) {
             LazyColumn(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f),
                 verticalArrangement = Arrangement.spacedBy(8.dp),
                 contentPadding = PaddingValues(vertical = 8.dp)
             ) {
@@ -51,10 +56,20 @@ fun GroupsOverviewScreen(
             Text("No groups found.")
         }
 
+        HorizontalDivider()
+
         Spacer(modifier = Modifier.height(8.dp))
 
-        IconButton(onClick = onCreateGroupClick) {
-            Icon(Icons.Default.AddCircle, contentDescription = "add a group")
+        TextButton(
+            onClick = onCreateGroupClick,
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Icon(Icons.Default.AddCircle, contentDescription = "add a group")
+                Text("create a new group")
+            }
         }
     }
 }
